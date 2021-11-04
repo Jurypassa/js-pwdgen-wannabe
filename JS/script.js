4aa8957b35de29db241d8f21d73daba7d5ca386d
Jury <jurypassa@gmail.com>
2021-11-04 16:08:45 +0100

    esercizio

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+const nome = prompt("inserisci il nome");
+console.log(nome);
+
+const cognome = prompt("Inserisci il cognome");
+console.log(cognome);
+
+const colore = prompt("Inserisci il tuo colore preferito");
+console.log(colore);
+
+let numero = 21
+console.log(numero);
+
+let test = nome + cognome + colore + 21
+document.getElementById("prova").innerHTML = test;
+console.log(test);
