96241b22d9730d8db8f70ea06b4ba98c219aee56
Jury <jurypassa@gmail.com>
2021-11-05 00:22:42 +0100

    Bonus

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,30 @@
+// Esercizio
+
+// const nome = prompt("inserisci il nome");
+// console.log(nome);
+
+// const cognome = prompt("Inserisci il cognome");
+// console.log(cognome);
+
+// const colore = prompt("Inserisci il tuo colore preferito");
+// console.log(colore);
+
+// let numero = 21; 
+// console.log(numero);
+
+// let test = nome + cognome + colore + 21;
+// document.getElementById("prova").innerHTML = test;
+// console.log(test);
+
+
 const nome = prompt("inserisci il nome");
+document.getElementById("nome").innerHTML = nome;
 console.log(nome);
 
 const cognome = prompt("Inserisci il cognome");
+document.getElementById("cognome").innerHTML = cognome;
 console.log(cognome);
 
-const colore = prompt("Inserisci il tuo colore preferito");
-console.log(colore);
-
-let numero = 21
-console.log(numero);
-
-let test = nome + cognome + colore + 21
-document.getElementById("prova").innerHTML = test;
-console.log(test);
+const data = prompt("Inserisci la data");
+document.getElementById("data").innerHTML = data;
+console.log(data);
